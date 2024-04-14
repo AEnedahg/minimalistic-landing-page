a9f3aa29c0c649f7675a7cb739296c15526774a4
Anthony Enedah <enedahanthony54@gmail.com>
2024-04-14 16:43:25 +0100

    Updated style.css and index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.html',
-  './dist/**/*.js'],
+  content: ['*.html',
+  '*.js'],
   theme: {
     width: {
       'xs': '320px',
